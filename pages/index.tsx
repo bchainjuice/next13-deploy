f9cebae5fabdddc82aae531449fbc853dc0dc52b
Bchain Tech <119753326+bchainjuice@users.noreply.github.com>
2023-10-12 09:28:42 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,68 +1,61 @@
-import { NextPage } from "next";
-import styles from "../styles/Home.module.css";
 import { MediaRenderer, Web3Button, useActiveClaimCondition, useAddress, useContract, useContractMetadata } from "@thirdweb-dev/react";
-import { ERC721_CONTRACT_ADDRESS } from "../const/addresses";
+import { ERC1155_CONTRACT_ADDRESS } from "../const/addresses";
+import styles from "../styles/Home.module.css";
 import Image from "next/image"
 
+export default function ERC1155() {
+    const address = useAddress();
 
-const Home: NextPage = () => {
-  const address = useAddress();
+    const {
+        contract: ERC1155Contract
+    } = useContract(ERC1155_CONTRACT_ADDRESS, "edition-drop");
 
-  const {
-    contract: ERC721Contract
-  } = useContract(ERC721_CONTRACT_ADDRESS);
-  const {
-    data: ERC721ContractMetadata,
-    isLoading: ERC721ContractMetadataIsLoading
-  } = useContractMetadata(ERC721Contract);
+    const {
+        data: ERC1155ContractMetadata,
+        isLoading: ERC1155ContractMetadataIsLoading
+    } = useContractMetadata(ERC1155Contract);
 
-  const {
-    data: ERC721ClaimCondition,
-    isLoading: ERC721ClaimConditionIsLoading
-  } = useActiveClaimCondition(ERC721Contract);
+    const {
+        data: ERC1155ClaimCondition
+    } = useActiveClaimCondition(ERC1155Contract, 0);
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.heroContainer}>
-        <div>
-          <h3>
-            <span className={styles.gradientText1}>
-            MEMBERSHIP PASS
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.heroContainer}>
+                <div>
+                <h3>
+                <span className={styles.gradientText1}>
+                MEMBERSHIP[ PASS ]
             </span>
-            
             </h3>
-            <h5>Grant testers access to Incentivized testnet.</h5>
-
-
-          <p>Cost: {ERC721ClaimCondition?.currencyMetadata.displayValue} {ERC721ClaimCondition?.currencyMetadata.symbol}</p>
-          {address ? (
-            <Web3Button
-              contractAddress={ERC721_CONTRACT_ADDRESS}
-              action={(contract) => contract.erc721.claim(1)}
-              onSuccess={() => alert("Claimed NFT")}
-            >Claim
-            </Web3Button>
-          ) : (
-            <p>Connect Wallet to claim!</p>
-          )}
-        </div>
-        <div className={styles.heroImageContainer}>
-          {!ERC721ContractMetadataIsLoading ? (
-            <div className={styles.heroImage}>
-              <MediaRenderer
-                src={ERC721ContractMetadata?.image}
-                height="70%"
-                width="70%"
-              />
-              <p>{ERC721ContractMetadata?.name}</p>
+                <p>Grant testers access to incentivized testnet.</p>
+                <p>[Price]: {ERC1155ClaimCondition?.currencyMetadata.displayValue} {ERC1155ClaimCondition?.currencyMetadata.symbol}</p>
+                {address ? (
+                    <Web3Button
+                    contractAddress={ERC1155_CONTRACT_ADDRESS}
+                    action={(contract) => contract.erc1155.claim(0, 1)}
+                    onSuccess={() => alert("Claimed NFT")}
+                    >claim<Image src="/icons/eagle.gif" alt="" width={30} height={30}/></Web3Button>
+                ) : (
+                    <p>Connect Wallet to claim!</p>
+                )}
+                </div>
+                <div className={styles.heroImageContainer}>
+                {!ERC1155ContractMetadataIsLoading ? (
+                    <div className={styles.heroImage}>
+                    <MediaRenderer
+                        src={ERC1155ContractMetadata?.image}
+                        height="80%"
+                        width="80%"
+                    />
+                    <p>{ERC1155ContractMetadata?.name}</p>
+                    </div>
+                ) : (
+                    <p><Image src="/icons/loading.gif" alt="" width={150} height={150}></Image></p>
+                )}
+                </div>
             </div>
-          ) : (
-            <p><Image src="/icons/loading.gif" alt="" width={150} height={150}/></p>
-          )}
         </div>
-      </div>
-    </div>
-  );
+    );
 };
-
-export default Home;
