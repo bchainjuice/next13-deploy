764f00500d2549a23a9fd3942794a2cc08678062
Bchain Tech <119753326+bchainjuice@users.noreply.github.com>
2023-10-22 01:05:12 +0100

    Update erc20.tsx

--- pages/erc20.tsx
+++ pages/erc20.tsx
@@ -30,7 +30,7 @@ export default function ERC20Project() {
     
     return (
         <div className= {styles.container}>
-       <Image src="/icons/chains.gif" alt="" width={550} height={550}/>
+       <Image src="/icons/chains.png" alt="" width={450} height={350}/>
             <div className={styles.grid}>
                  <div className={styles.componentCard}>
                       {tokenSupplyIsLoading ? (
@@ -46,7 +46,7 @@ export default function ERC20Project() {
                             <p></p>
                       )}
                 </div>
-       <Image src="/icons/coming.gif" alt="" width={350} height={350}/>
+       <Image src="/icons/coming.gif" alt="" width={450} height={350}/>
             </div>
             
        <p><Image src="/icons/arb.png" alt="" width={190} height={190}/></p>
