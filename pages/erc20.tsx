ae69ae47ccdacc0320f0712758fda6bf919b3417
Bchain Tech <119753326+bchainjuice@users.noreply.github.com>
2023-10-23 21:30:57 +0100

    Update erc20.tsx

--- pages/erc20.tsx
+++ pages/erc20.tsx
@@ -46,6 +46,8 @@ export default function ERC20Project() {
                             <p></p>
                       )}
                 </div>
+                
+       <p><Image src="/icons/arb.png" alt="" width={150} height={150}/> </p>
             </div>
         </div>
     )
